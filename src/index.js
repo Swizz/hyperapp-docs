9a1957c796cca9fccb65d57cf2a0edfb4507181c
Swizz <gerodel.quentin@gmail.com>
2017-09-28 20:14:49 +0200

    Fetch docs

--- src/index.js
+++ src/index.js
@@ -1,52 +1,102 @@
 import { h, app } from "hyperapp"
-import { main, svg, h1, span, a as link, button } from "@hyperapp/html"
+import { main, nav, section, select, option, div, span } from "@hyperapp/html"
+
+import snarkdown from 'snarkdown'
 
 import "./index.css"
 
-const icon = href => svg([use(href)])
-const use = href =>
-  h("use", {
-    oncreate: e =>
-      e.setAttributeNS(
-        "http://www.w3.org/1999/xlink",
-        "href",
-        "/icons.svg#" + href
-      )
-  })
+const REPO = "hyperapp/hyperapp"
+const API_HOST = "https://api.github.com"
+const RAW_HOST = "https://raw.githubusercontent.com"
+
+const replaceLinks = (content, path) =>
+  content.replace(
+    /\/docs\/(.+)\.md/g,
+    path.base.concat(path.release, "$1").join("/")
+  )
 
 app({
-  state: {},
-  actions: {},
-  events: {},
-  view: (s, a) =>
-    main([
-      icon("hyperapp"),
-      h1("1 KB JavaScript library for building frontend applications"),
-      h("row-", { class: "social", gap: "1" }, [
-        link({ href: "https://github.com/hyperapp/hyperapp" }, [
-          icon("github"),
-          span("GITHUB")
-        ]),
-        link({ href: "https://hyperappjs.herokuapp.com" }, [
-          icon("slack"),
-          span({}, "SLACK")
-        ]),
-        link({ href: "https://twitter.com/hyperappjs" }, [
-          icon("twitter"),
-          span("TWITTER")
-        ])
+  state: {
+    path: {
+      base: location.pathname.split("/").slice(0, 2),
+      release: location.pathname.split("/")[2],
+      document: location.pathname.split("/").slice(3)
+    },
+    releases: [],
+    content: "",
+    menu: ""
+  },
+  actions: {
+    selectRelease(state, actions, event) {
+      const release = event.target.value
+      location.pathname = state.path.base.concat(release, state.path.document).join("/")
+    },
+    fetch: {
+      releases(state, actions) {
+        return update => {
+          fetch(`${API_HOST}/repos/${REPO}/releases`)
+            .then(data => data.json())
+            .then(
+              releases => update({ releases: releases.map(release => release.tag_name) })
+            )
+        }
+      },
+      content(state, actions) {
+        return update => {
+          fetch(`${RAW_HOST}/${REPO}/${state.path.release}/docs/${state.path.document.join("/")}.md`)
+            .then(data => data.text())
+            .then(
+              content => update({ content })
+            )
+        }
+      },
+      menu(state, actions) {
+        return update => {
+          fetch(`${RAW_HOST}/${REPO}/${state.path.release}/docs/README.md`)
+            .then(data => data.text())
+            .then(
+              content => update({ menu: content })
+            )
+        }
+      }
+    }
+  },
+  events: {
+    load(state, actions) {
+      actions.fetch.releases()
+      actions.fetch.menu()
+      actions.fetch.content()
+    },
+    update(state, actions) {
+      if (state.releases.length === 0) {
+        return
+      }
+      if (!state.path.release || !state.path.document || state.path.document[0] === "") {
+        location.pathname = state.path.base.concat(
+          !state.path.release ? state.releases[0] : state.path.release,
+          !state.path.document.length || state.path.document[0] === "" ? "getting-started" : state.path.document
+        ).join("/")
+      }
+    }
+  },
+  view(state, actions) {
+    return main([
+      nav([
+        span("release : "),
+        select({ onchange: actions.selectRelease }, state.releases.map(
+          release => option({ selected: release == state.path.release }, release)
+        )),
+        div({
+          onupdate: element => {
+            element.innerHTML = snarkdown(replaceLinks(state.menu, state.path))
+          }
+        }),
       ]),
-      h("row-", { class: "reading", gap: "1" }, [
-        link({ href: "https://github.com/hyperapp/hyperapp/tree/master/src" }, [
-          span("READ THE CODE")
-        ]),
-        link(
-          {
-            href:
-              "https://github.com/hyperapp/hyperapp/blob/master/docs/README.md#documentation"
-          },
-          [span("READ THE DOCS")]
-        )
-      ])
+      section({
+        onupdate: element => {
+          element.innerHTML = snarkdown(replaceLinks(state.content, state.path))
+        }
+      })
     ])
+  }
 })
